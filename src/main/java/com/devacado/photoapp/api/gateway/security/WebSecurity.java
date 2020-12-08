cd9958e9ebfcee31aa1e3dc980ab549fce39a7fa
KayTee9 <tajfar.khatere@yahoo.com>
2020-12-08 17:11:30 +0100

    17. Spring Boot Actuator - A Quick Start

--- src/main/java/com/devacado/photoapp/api/gateway/security/WebSecurity.java
+++ src/main/java/com/devacado/photoapp/api/gateway/security/WebSecurity.java
@@ -25,7 +25,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.headers().frameOptions().disable();
         http.authorizeRequests()
-                .antMatchers(HttpMethod.POST, environment.getProperty("api.h2console.url.path")).permitAll()
+                .antMatchers(environment.getProperty("api.users.actuator.url.path")).permitAll()
+                .antMatchers(environment.getProperty("api.zuul.actuator.url.path")).permitAll()
+                .antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
                 .antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
                 .antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
                 .anyRequest().authenticated();
